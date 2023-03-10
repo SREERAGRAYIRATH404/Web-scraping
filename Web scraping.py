9316a281d370e1d2bb4f8c4868f104980735746f
SREERAGRAYIRATH404 <90212360+SREERAGRAYIRATH404@users.noreply.github.com>
2023-03-11 01:42:47 +0530

    web scraping
    A program to find the meaning of a word using web scraping

---
+++ Web scraping.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+word = input("Enter a word to search for: ")
+url = f"https://www.dictionary.com/browse/{word}"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+definition = soup.find("div", {"class": "css-1ghs5zt e1q3nk1v3"})
+if definition:
+    meaning = definition.text.strip()
+    print(f"The meaning of '{word}' is: {meaning}")
+else:
+    print(f"No definition found for '{word}'")
